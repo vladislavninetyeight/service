25fedad6d44caaae1b2914cc6bf93853cc70dbff
vladislave <vladislavninetyeight@gmail.com>
2024-07-14 08:52:52 +0300

    Fix post convertor at repository

--- internal/repositories/post/converter/post.go
+++ internal/repositories/post/converter/post.go
@@ -1,3 +1,17 @@
 package converter
 
+import (
+	"github.com/vladislavninetyeight/service/internal/model"
+	rep "github.com/vladislavninetyeight/service/internal/repositories/post/model"
+)
 
+func FromPostDetailToRep(id uint, detail model.PostDetail) rep.Post {
+	return rep.Post{
+		ID: id,
+		Detail: rep.PostDetail{
+			Title:  detail.Title,
+			Body:   detail.Body,
+			UserID: detail.UserID,
+		},
+	}
+}
